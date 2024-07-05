5fad38ac60dbfdda042fb9773480d694109f2029
seollove79 <seollove@gmail.com>
2024-07-05 10:12:54 +0900

    test

--- lib/config.py
+++ lib/config.py
@@ -16,10 +16,10 @@ CLASSES = ("person", "bicycle", "car", "motorbike ", "aeroplane ", "bus ", "trai
 DEVICE_COMPATIBLE_NODE = '/proc/device-tree/compatible'
 
 RK356X_RKNN_MODEL = 'models/yolov5s.rknn'
-RK3588_RKNN_MODEL = 'models/yolov5s-640-640.rknn'
+#RK3588_RKNN_MODEL = 'models/yolov5s-640-640.rknn'
 #RK3588_RKNN_MODEL = 'models/yolov5s_relu.rknn'
 #RK3588_RKNN_MODEL = 'yolov5s_relu_tk2_RK3588_i8'
-#RK3588_RKNN_MODEL = 'models/yolov5n.rknn'
+RK3588_RKNN_MODEL = 'models/yolov5n.rknn'
 
 #Webcam dev /device/video0, /device/video1 etc.
 CAM_DEV = 0
